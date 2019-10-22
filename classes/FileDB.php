a15bec1a427cab42acbb973b2f842931d305f44b
Jonas <jakimaviciusjonas@gmail.com>
2019-10-22 12:19:49 +0300

    PHP OOP. 2 užduotis. Papildyta file_to_array funkcija

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -1,16 +1,19 @@
 <?php
 
+require('functions/file.php');
+
 class FileDB
 {
-
     private $file_name;
+    private $data;
 
     public function __construct($file_name)
     {
         $this->file_name = $file_name;
     }
+
+    public function load()
+    {
+        $this->data = file_to_array($file_name);
+    }
 }
-
-$index = new FileDB('index.php');
-
-var_dump($index);
--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 
 require ('classes/FileDB.php');
 
+$index = new FileDB('index.php');
 
+var_dump($index);
 
